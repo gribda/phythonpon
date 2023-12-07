e8e90672b0712520a4e32b63563129df3c45992b
gribda <gerbx7428@gmail.com>
2023-12-07 18:48:21 +0300

    07.12.2023

--- shtoooof.py
+++ shtoooof.py
@@ -1,1 +1,27 @@
-print("hello")
+class Student:
+    count = 0
+    def __init__(self, name="No name", height=160):
+        self.height = height
+        self.name = name
+        Student.count += 1
+
+    def __str__(self):
+        return f"Я студент  {self.name}.\nМій зріст {self.height} см"
+
+    def __del__(self):
+        print("Я ухожу")
+
+
+
+
+
+print(Student.count)
+student = Student("Vitalik", 170)
+print(student)
+
+#student1 = Student(height=170)
+#student2 = Student(height=150)
+#print(student.height)
+#print(student1.height)
+#print(student2.height)
+#print(student2.count)
